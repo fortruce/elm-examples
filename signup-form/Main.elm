cb07d46a9c8ff593b45828b3446951c95a15a2b9
Joseph Rollins <joseph@socialtables.com>
2016-01-03 22:15:50 -0500

    Finish the noredink signup-form walkthrough

--- signup-form/Main.elm
+++ signup-form/Main.elm
@@ -1,20 +1,41 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, on, targetValue)
+import String
+import StartApp
+import Effects
+import Http
+import Task
+import Json.Decode exposing (succeed)
 
 
-type alias Model =
+type alias Errors =
   { username : String
   , password : String
   }
 
 
-init : Model
-init =
-  Model "" ""
+type alias Model =
+  { username : String
+  , password : String
+  , errors : Errors
+  }
 
 
-view : Model -> Html
-view model =
+initialModel : Model
+initialModel =
+  Model "" "" initialErrors
+
+
+initialErrors : Errors
+initialErrors =
+  { username = ""
+  , password = ""
+  }
+
+
+view : Signal.Address Action -> Model -> Html
+view address model =
   Html.form
     [ id "signup-form" ]
     [ h1 [] [ text "Sensational Signup Form" ]
@@ -23,15 +44,115 @@ view model =
         [ id "username-field"
         , type' "text"
         , value model.username
+        , on "input" targetValue (\v -> Signal.message address (SetUsername v))
         ] []
+
+    , div [ class "validation-error" ] [ text model.errors.username ]
+
     , label [ for "password" ] [ text "password: " ]
     , input
         [ id "password-field"
         , type' "password"
         , value model.password
+        , on "input" targetValue (\v -> Signal.message address (SetPassword v))
         ] []
-    , div [ class "signup-button" ] [ text "Sign Up!" ]
+
+    , div [ class "validation-error" ] [ text model.errors.password ]
+
+    , div
+        [ class "signup-button"
+        , onClick address Validate ] [ text "Sign Up!" ]
     ]
 
+
+getErrors : Model -> Errors
+getErrors model =
+  { username =
+      if String.isEmpty(model.username) then
+        "Please enter a username!"
+      else
+        ""
+  , password =
+      if String.isEmpty(model.password) then
+        "Please enter a password!"
+      else
+        ""
+  }
+
+
+type Action
+  = Validate
+  | SetUsername String
+  | SetPassword String
+  | UsernameCheck (Maybe Bool)
+
+
+withUsernameTaken : Bool -> Model -> ( Model, Effects.Effects Action )
+withUsernameTaken isTaken model =
+  if isTaken then
+    let
+      { errors } = model
+      newErrors = { errors | username = "That username is already taken!" }
+    in
+      ( { model | errors = newErrors }, Effects.none )
+  else
+    ( model, Effects.none )
+
+
+update : Action -> Model -> (Model, Effects.Effects Action)
+update action model =
+  case action of
+    Validate ->
+      let
+        url = "https://api.github.com/users/" ++ model.username
+        request =
+          Http.get (succeed True) url
+            |> Task.toMaybe
+            |> Task.map UsernameCheck
+            |> Effects.task
+      in
+        ( { model |
+              errors = getErrors model
+          }
+        , request
+        )
+
+    SetUsername rawInput ->
+      ( { model |
+            username = rawInput
+        }
+      , Effects.none
+      )
+
+    SetPassword rawInput ->
+      ( { model |
+            password = rawInput
+        }
+      , Effects.none
+      )
+
+    UsernameCheck success ->
+      case success of
+        Just b ->
+          withUsernameTaken True model
+
+        Nothing ->
+          withUsernameTaken False model
+
+
+app =
+  StartApp.start
+    { init = ( initialModel, Effects.none )
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+
 main =
-  view init
+  app.html
+
+
+port tasks : Signal (Task.Task Effects.Never ())
+port tasks =
+  app.tasks
